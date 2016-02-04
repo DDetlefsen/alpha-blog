e2381f9d5167800706d6c6ba8dbd117e029d8a26
David Detlefsen <Detlefsen@gmail.com>
2016-02-03 22:50:44 -0600

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
 
+  resources :categories, except: [:destory]
+
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
